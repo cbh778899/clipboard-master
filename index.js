d5b928a88a5efed9084b7677e10771383861ddeb
cbh778899 <cbh778899@outlook.com>
2025-03-07 10:34:03 +1100

    rearrange code to make it more readable
    Signed-off-by: cbh778899 <cbh778899@outlook.com>

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // ===========================================
 // INITIALIZE
 // ===========================================
+
+// basic imports
 require("dotenv").config()
 const { join } = require('path')
 
+// init express
 const express = require("express");
 const https = require('https');
 const app = express();
 app.use(require('cors')());
+// import websocket
 const expressWs = require('express-ws');
-app.use(express.static(join(__dirname, 'dist')));
 const bodyParserJSON = require('body-parser').json();
-
+// database
 require('./database').init();
 
+// init folders
 const { upload, get, deleteItem } = require('./actions/files');
 const middleware = require('./utils/multer-config');
-
 const FILE_SAVE_PATH = process.env.FILE_SAVE_PATH || 'uploads/';
 const { existsSync, mkdirSync, readFileSync } = require('fs');
 if (!existsSync(FILE_SAVE_PATH)) {
     mkdirSync(FILE_SAVE_PATH);
 }
 
+// init https settings if applicable
 let server = null;
 const secureConnection = existsSync('cert');
 if (secureConnection) {
@@ -52,6 +56,7 @@ app.ws('/api/ws', require('./actions/ws').wsHandler)
 // Base routers
 // ===========================================
 app.use('/api/files', filesRouter);
+app.use(express.static(join(__dirname, 'dist')));
 app.get('*', (_, res)=>{
     res.sendFile(join(__dirname, 'dist', 'index.html'));
 })
